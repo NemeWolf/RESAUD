b1ea20ca5ac23c07c6c70809611dcefe3a5275e3
NemeWolf <nehemiasman_8@live.com>
2023-11-13 00:15:10 -0300

    Tkinter funcionando

--- pruebas.py
+++ pruebas.py
@@ -1,12 +1,33 @@
-from tkinter import Button, Tk
+from tkinter import Text, Button, Tk
 
 ventana = Tk()
 
-def cambiar_estado(estado):
-  button.config(state=estado)
+# Crea la ventana de texto
+ventana_texto = Text(ventana)
 
-button = Button(ventana, text="Cambiar estado", command=lambda: cambiar_estado("normal"))
-button.pack()
+# Crea el cuadro de entrada
+cuadro_entrada = input(ventana)
+
+# Crea el botón
+boton = Button(ventana, text="Insertar texto")
+
+# Asigna el manejador de eventos al botón
+boton.bind("<Button-1>", "insertar_texto")
+
+# Muestra la ventana de texto
+ventana_texto.pack()
+
+# Muestra el cuadro de entrada
+cuadro_entrada.pack()
+
+# Muestra el botón
+boton.pack()
+
+def insertar_texto():
+  # Lee el texto del cuadro de entrada
+  texto = cuadro_entrada.get()
+
+  # Inserta el texto en la ventana de texto
+  ventana_texto.insert("1.0", texto)
 
 ventana.mainloop()
-
