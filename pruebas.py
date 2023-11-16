6bdd83b03b81627ff436e25239f231a28f1c0ba0
NemeWolf <nehemiasman_8@live.com>
2023-11-16 03:37:52 -0300

    Ultimos ajustes

--- pruebas.py
+++ pruebas.py
@@ -1,33 +1,134 @@
-from tkinter import Text, Button, Tk
+from tkinter import *
+import pandas as pd
+import whisper
+from whisper.utils import get_writer
+import os
+from openai import OpenAI
 
-ventana = Tk()
+#Cargamos key openai
+client = OpenAI(
+  api_key="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",
+)
 
-# Crea la ventana de texto
-ventana_texto = Text(ventana)
-
-# Crea el cuadro de entrada
-cuadro_entrada = input(ventana)
-
-# Crea el botón
-boton = Button(ventana, text="Insertar texto")
-
-# Asigna el manejador de eventos al botón
-boton.bind("<Button-1>", "insertar_texto")
-
-# Muestra la ventana de texto
-ventana_texto.pack()
-
-# Muestra el cuadro de entrada
-cuadro_entrada.pack()
-
-# Muestra el botón
-boton.pack()
-
-def insertar_texto():
-  # Lee el texto del cuadro de entrada
-  texto = cuadro_entrada.get()
-
+#TRANSCRIBIR
+def transcribir(modo:str, path:str, ventana, ventana_texto01): 
+  ventana_texto01.delete("1.0", "end")
   # Inserta el texto en la ventana de texto
-  ventana_texto.insert("1.0", texto)
+  ventana_texto01.insert("1.0", "Trancribiendo...")
+  # Actualiza la ventana de Tkinter
+  ventana.update() 
+  
+  #transcripción
+  model = whisper.load_model(modo) #tiny(1gb); base(1gb); small(2gb); medium(5gb); large(10gb)
+  result = model.transcribe(path)
+
+  global Transcripcion
+  Transcripcion =  result
+  
+  # Inserta el texto en la ventana de texto
+  ventana_texto01.delete("1.0", "end")
+  ventana_texto01.insert("1.0", Transcripcion['text'])
+  ventana.update()    
+
+  return result
+
+
+#RESUMIR
+def resumir(model:str,Transcripcion:str, ventana, ventana_texto02, prompt_type):
+  ventana_texto02.delete("1.0", "end")
+  ventana_texto02.insert("1.0", "Resumiendo...")
+  # Actualiza la ventana de Tkinter
+  ventana.update() 
+  
+  if size_selector.get() == "Largo":
+    texto_size = 15
+  if size_selector.get() == "Medio":
+    texto_size = 10
+  if size_selector.get() == "Corto":
+    texto_size = 5
+  
+  #Calculamos proporcion del resumen
+  text = Transcripcion['text']
+  
+  words_counter = len(text.split())
+  sumary_size = round((words_counter*texto_size) /100)
+      
+  if prompt_selector.get():
+    prompt_type = prompt_selector.get()
+      
+      
+  
+  if prompt_type == "Academico":
+    #ACADEMICO
+    prompt = f"Your task is to synthesize a comprehensive report based on transcribed text from a university lecture. Your report should be about {sumary_size} words. Your report should distill the core subject matter, enumerate the key concepts discussed, and catalog noteworthy comments made during the session. Please proceed to construct a detailed report on the following text: {text}"
+  
+  if prompt_type == "Narrativo":
+    #NARRATIVO
+    prompt = f"Your task is to synthesize a comprehensive report based on transcribed text from a narrative audio. Your report should be about {sumary_size} words. It should distill the core subject matter, enumerate the key concepts discussed, and catalog noteworthy comments made during the session. Please proceed to construct a detailed report on the following text: {text}"
+  
+  if prompt_type == "Noticia":
+    #NOTICIAS
+    prompt = f"Your task is to generate a concise news summary based on the provided text from recent news articles. Your summary should be approximately {sumary_size} words. Condense the core information, highlight key events, and outline notable details discussed in the articles. Please proceed to craft an informative summary of the following news text: {text}"
+
+  message = [
+      {"role": "system", "content": "You are an advanced information collector and synthesizer."},
+      {"role": "user", "content": prompt}] 
+  
+  response01 = client.chat.completions.create(
+      messages=message,
+      model=model,
+      temperature = 0,
+  )
+  global sumary
+  sumary =  response01
+  
+  # Inserta el texto en la ventana de texto
+  ventana_texto02.delete("1.0", "end")
+  ventana_texto02.insert("1.0", sumary.choices[0].message.content)
+  # Actualiza la ventana de Tkinter
+  ventana.update()    
+  return response01
+  
+  
+#TKINTER
+  
+  
+from tkinter import filedialog
+from tkinter import ttk
+ventana = Tk()
+#ventana.geometry("500x450")
+
+ruta = None
+texto_size = ""
+prompt_type = "Academico"
+
+def openFile():
+  global ruta
+  ruta = filedialog.askopenfilename(initialdir="N:/MisArchivos/Universidad/4toaño/2doSemestre/PISS_/Audios")
+  #filedialog.askopenfilename(initialdir=os.getcwd()) 
+    
+ventana_texto01 = Text(ventana)
+ventana_texto01.config(width=60, height=10)
+
+ventana_texto02 = Text(ventana)
+ventana_texto02.config(width=60, height=10)
+
+buttonOpenFile = Button(text="Selecciona tu audio",command=openFile)
+
+buttonTranscribe = Button(text="Transcribir", command=lambda: transcribir("small", ruta,ventana, ventana_texto01))
+
+prompt_selector = ttk.Combobox(ventana, values= ["Academico", "Narrativo", "Noticia"], width=10, height=1)
+
+size_selector = ttk.Combobox(ventana, values=["Largo", "Medio", "Corto"], width=10, height=1)
+
+buttonResumir = Button(text="Resumir", command=lambda: resumir('gpt-4-1106-preview',Transcripcion, ventana, ventana_texto02, prompt_type))
+
+ventana_texto01.grid(row=0, column=0, columnspan=6,padx=10, pady=10)
+ventana_texto02.grid(row=1, column=0, columnspan=6,padx=10, pady=10)
+buttonOpenFile.grid(row=2, column=1, padx=10, pady=10)
+buttonTranscribe.grid(row=2, column=2, padx=10, pady=10)
+prompt_selector.grid(row=2, column=3, padx=10, pady=10)
+size_selector.grid(row=2, column=4, padx=10, pady=10)
+buttonResumir.grid(row=2, column=5, padx=10, pady=10)
 
 ventana.mainloop()
